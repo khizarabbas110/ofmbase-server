ac6bf36750557f2f2980f7148809a8e5f2934fad
Khizar-Abbas-Developer <khizarnot7@gmail.com>
2025-07-03 09:18:07 +0500

    4ceffd

--- utils/transporter.js
+++ utils/transporter.js
@@ -2,7 +2,9 @@ import nodemailer from "nodemailer";
 import "dotenv/config";
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail", // ✅ Gmail SMTP
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
